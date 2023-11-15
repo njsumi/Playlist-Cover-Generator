26580ebe62b45a48a4ee7b0c6828d480d72fb06b
Nina Gruteser <ngruteser@icloud.com>
2023-11-15 13:52:11 -0500

    generated image appears on webpage

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import base64
 import json
 from requests import post, get
 import openai
+from matplotlib import pyplot as plt
+from matplotlib import image as mpimg
+import urllib.request
+from PIL import Image
+import numpy as np
+
 
 load_dotenv()
 
@@ -48,7 +54,7 @@ def get_tracks_audio_features(token, track_ids):
 
 token = get_token()
 
-playlist_url = input("Enter Spotify Playlist URL: ") #"https://open.spotify.com/playlist/3BMylbkttS9hqjOsqoXwJT?si=0686e92cbe6345dd"
+playlist_url = "https://open.spotify.com/playlist/69DtCPWWYsZ7Zaq6avlbNH?si=74c5b6bd3e814cfa&nd=1"
 playlist_id = playlist_url[34:56]
 
 tracks = get_playlist_tracks(token, "3BMylbkttS9hqjOsqoXwJT") # ?si=860de0e89be048bc
@@ -58,7 +64,7 @@ track_ids = ",".join(track['track']['id'] for track in tracks)
 #print(track_ids)
 
 audio_features = get_tracks_audio_features(token, track_ids)
-
+print("hello")
 #print(audio_features)
 # for track in audio_features['audio_features']:
 #     danceability = track['danceability']
@@ -110,27 +116,11 @@ avg_tempo = sum(tempo)/len(tempo)
 avg_time_signature = sum(time_signature)/len(time_signature)
 avg_valence = sum(valence)/len(valence)
 
-print("acousticness: ", avg_acousticness, " danceability: ", avg_danceability, " energy: ", avg_energy, " instrumentalness: ",avg_instrumentalness, " key: ",avg_key, 
-      " liveness: ", avg_liveness, " mode: ",avg_mode, " speechiness: ",avg_speechiness, " tempo: ",avg_tempo, " time signature: ",avg_time_signature, " valence: ",avg_valence)
-
-# print("acousticness: ", avg_acousticness)
-# print("danceability: ", avg_danceability)
-# print("energy: ", avg_energy)
-# print("instrumentalness: ", avg_instrumentalness)
-# print("key: ", avg_key)
-# print("liveness: ", avg_liveness)
-# print("mode: ", avg_mode)
-# print("speechiness: ", avg_speechiness)
-# print("tempo: ", avg_tempo)
-# print("time signature: ", avg_time_signature)
-# print("valence: ", avg_valence)
-
-prompt = """in three to eight words, give a pinterest search prompt for a playlist cover for a playlist with an average acousticness of {avg_acousticness} (range: 0-1), danceability of {avg_danceability} range: 0-1), energy of {avg_energy} (range: 0-1), instrumentalness of {avg_instrumentalness} (range: 0-1), key of {avg_key} (range: -1-11), liveness of {avg_liveness} (range: 0-1), mode of {avg_mode}, speechiness of {avg_speechiness} (range: 0-1), tempo of {avg_tempo} bpm, time signature of {avg_time_signature} (range: 3-7), and valence {avg_valence} (range: 0-1).""".format(avg_acousticness=avg_acousticness, avg_danceability=avg_danceability, 
+prompt = """in three to eight words describe a playlist with an average acousticness of {avg_acousticness} (range: 0-1), danceability of {avg_danceability} range: 0-1), energy of {avg_energy} (range: 0-1), instrumentalness of {avg_instrumentalness} (range: 0-1), key of {avg_key} (range: -1-11), liveness of {avg_liveness} (range: 0-1), mode of {avg_mode}, speechiness of {avg_speechiness} (range: 0-1), tempo of {avg_tempo} bpm, time signature of {avg_time_signature} (range: 3-7), and valence {avg_valence} (range: 0-1).""".format(avg_acousticness=avg_acousticness, avg_danceability=avg_danceability, 
                                                                             avg_energy=avg_energy, avg_instrumentalness=avg_instrumentalness, avg_key=avg_key, 
                                                                             avg_liveness=avg_liveness, avg_mode=avg_mode, avg_speechiness=avg_speechiness, 
                                                                             avg_tempo=avg_tempo, avg_time_signature=avg_time_signature, avg_valence=avg_valence)
-
-#print(prompt); 
+print(prompt)
 
 openai.api_key = os.getenv("API_KEY")
 
@@ -150,7 +140,21 @@ def get_openai_response(prompt):
 
 #convert promt to lowercase and spaces are %20
 response = get_openai_response(prompt).lower()
+response += " image in studio ghibli style with no text"
 print(response)
+
+image_response = openai.Image.create(
+  prompt="Acoustic guitar with gentle rhythmic movement image in studio ghibli style",
+  n=1,
+  size="1024x1024"
+)
+image_url = image_response['data'][0]['url']
+
+image = Image.open(urllib.request.urlopen(image_url))
+image_array = np.array(image)
+
+plt.imshow(image_array)
+plt.show()
 
 
 
--- playlistcovergenerator/app/views.py
+++ playlistcovergenerator/app/views.py
@@ -1,9 +1,170 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from dotenv import load_dotenv
+import os
+import base64
+import json
+import requests
+from requests import post, get
+import openai
+import string
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
 
 # Create your views here.
 def index(request):
     context = {
         'name': 'Nina'
     }
+
+    if request.method == 'POST':
+        playlist_url = request.POST.get('url', '')
+        if playlist_url:
+            playlist_id = playlist_url[34:56]
+            audio_features = get_playlist_info(playlist_id)
+            openai_prompt = generate_openai_prompt(audio_features)
+            print(openai_prompt)
+            openai_response = get_openai_response(openai_prompt)
+            print(openai_response)
+            image_response =  generate_dalle_image(openai_response)
+            print(image_response)
+            context['image_url'] = image_response
+            
+
+
     return render(request, "index.html", context)
+
+
+def get_openai_response(prompt):  
+  openai.api_key = os.getenv("API_KEY")
+  result = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {
+        "role": "user",
+        "content": prompt
+      }
+    ]
+  )
+    
+  response_content = result.choices[0].message.content
+  translator = str.maketrans("", "", string.punctuation)
+  return response_content.translate(translator)
+
+def generate_dalle_image(openai_reponse):
+    prompt = openai_reponse + " image in the style of Hayao Miyazaki"
+    print(prompt)
+    image_response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size="1024x1024"
+    )
+    image_url = image_response['data'][0]['url']
+    return image_url
+
+
+def get_playlist_info(playlist_id):
+    token = get_token()
+    tracks = get_playlist_tracks(token, playlist_id)
+    track_ids = ",".join(track['track']['id'] for track in tracks)
+    audio_features = get_tracks_audio_features(token, track_ids)
+    return audio_features
+
+def generate_openai_prompt(audio_features):
+    acousticness = []
+    for track in audio_features['audio_features']:
+        acousticness.append(track['acousticness'])
+    danceability = []
+    for track in audio_features['audio_features']:
+        danceability.append(track['danceability'])
+    energy = []
+    for track in audio_features['audio_features']:
+        energy.append(track['energy'])
+    instrumentalness = []
+    for track in audio_features['audio_features']:
+        instrumentalness.append(track['instrumentalness'])
+    key = []
+    for track in audio_features['audio_features']:
+        key.append(track['key'])
+    liveness = []
+    for track in audio_features['audio_features']:
+        liveness.append(track['liveness'])
+    mode = []
+    for track in audio_features['audio_features']:
+        mode.append(track['mode'])
+    speechiness = []
+    for track in audio_features['audio_features']:
+        speechiness.append(track['speechiness'])
+    tempo = []
+    for track in audio_features['audio_features']:
+        tempo.append(track['tempo'])
+    time_signature = []
+    for track in audio_features['audio_features']:
+        time_signature.append(track['time_signature'])
+    valence = []
+    for track in audio_features['audio_features']:
+        valence.append(track['valence'])
+
+    avg_acousticness = sum(acousticness)/len(acousticness)
+    avg_danceability = sum(danceability)/len(danceability)
+    avg_energy = sum(energy)/len(energy)
+    avg_instrumentalness = sum(instrumentalness)/len(instrumentalness)
+    #avg_key = sum(key)/len(key)
+    avg_liveness = sum(liveness)/len(liveness)
+    #avg_mode = sum(mode)/len(mode)
+    #avg_speechiness = sum(speechiness)/len(speechiness)
+    avg_tempo = sum(tempo)/len(tempo)
+    avg_time_signature = sum(time_signature)/len(time_signature)
+    avg_valence = sum(valence)/len(valence)
+
+    openai_prompt = "in 3-8 words, what image or object represents a song with " + label_value(avg_acousticness, "accousticness") + ", " + label_value(avg_danceability, "danceability") + ", " + label_value(avg_energy, "energy") + ", " + label_value(avg_instrumentalness, "instrumentalness") + ", " + label_value(avg_liveness, "liveness") + ", tempo of " + str(avg_tempo) + " bpm, time signature of " + str(avg_time_signature) + ", and " + label_value(avg_valence, "valence") + "?"
+    return openai_prompt
+
+
+def label_value(value, name):
+    if 0.8 <= value <= 1:
+        return "high " + name
+    elif 0.4 <= value:
+        return "medium " + name
+    else:
+        return "low " + name
+
+
+def get_token():
+    authorization_string = client_id + ":" + client_secret
+    authorization_bytes = authorization_string.encode("utf-8")
+    authorization_base64 = str(base64.b64encode(authorization_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + authorization_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+
+    #convert json string into python dictionary to access info inside
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+def get_authorization_header(token):
+    return {"Authorization": "Bearer " + token}
+
+def get_playlist_tracks(token, playlist_id):
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
+    headers = get_authorization_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+    tracks = json_result.get('tracks', {}).get('items', [])
+    return tracks
+
+def get_tracks_audio_features(token, track_ids):
+    url = f"https://api.spotify.com/v1/audio-features?ids={track_ids}"
+    headers = get_authorization_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+    return json_result
